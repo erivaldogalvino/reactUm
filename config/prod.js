58f6edc331e1a6e69ab98901bd1bcab2e729b489
billperroni <billperroni@gmail.com>
2018-08-22 19:55:42 -0300

    arrumar prod heroku

--- config/prod.js
+++ config/prod.js
@@ -7,3 +7,6 @@ module.exports = {
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
+
+// yyyyyyyyyyyyyyyyyyyyy
+// xxxxxxxxxxxxx
